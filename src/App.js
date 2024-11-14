651fa6e6891c1c662d04d8216064b7d298cba2a4
Zhaoyi Yan <priscillayouziqian@gmail.com>
2024-11-14 15:58:32 -0500

    JobCard component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import JobCard from './JobCard';
+
+const jobList = [
+  {
+    id: 0,
+    name: "front-end developer",
+    company: "Apple",
+    status: null
+  },
+  {
+    id: 1,
+    name: "back-end developer",
+    company: "Google",
+    status: null
+  },
+  {
+    id: 2,
+    name: "full stack developer",
+    company: "TikTok",
+    status: null
+  }
+]
+
 function App() {
   return (
     <div>
-      <h1>My App</h1>
+      <h1>My Job Tracking App</h1>
+      {jobList.map(j => <JobCard job={j} />)}
     </div>
   );
 }
