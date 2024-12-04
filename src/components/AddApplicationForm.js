c1fd3e47c44ea498c213cf2a4ced7520aeb4d73f
Zhaoyi Yan <priscillayouziqian@gmail.com>
2024-12-03 19:57:22 -0500

    update alert msg to pop out modal

--- src/components/AddApplicationForm.js
+++ src/components/AddApplicationForm.js
@@ -1,11 +1,13 @@
-import { Button, Label, Col, FormGroup } from "reactstrap";
+import { Button, Label, Col, FormGroup, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { validateAddApplicationForm } from "../utils/validateAddApplicationForm";
 import SkillsCheckboxGroup from "./SkillsCheckboxGroup";
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const AddApplicationForm = () => {
     const navigate = useNavigate();
+    const [modal, setModal] = useState(false);
 
     const handleSubmit = (values, { resetForm }) => {
         // console.log('Application values: ', values);
@@ -22,147 +24,165 @@ const AddApplicationForm = () => {
         };
         console.log(application);
 
-        alert('New job application entered successfully!');
+        setModal(true); //show the success msg modal
         resetForm();
-        navigate('/');
     }
 
-  return (
-    <Formik
-      initialValues={{
-        name: "",
-        mode: "",
-        company: "",
-        status: "",
-        type: "",
-        skills: [],
-        jobLink: ""
-      }}
-      onSubmit={handleSubmit}
-      validate={validateAddApplicationForm}
-    >
-      <Form>
-        <FormGroup row>
-          <Label htmlFor="name" md="2">
-            Position Name
-          </Label>
-          <Col md="10">
-            <Field 
-                name='name'
-                placeholder='Position Name'
-                className="form-control" />
-            <ErrorMessage name="name">
-                {(msg) => <p className="text-danger">{msg}</p>}
-            </ErrorMessage>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label htmlFor="mode" md="2">
-            Mode
-          </Label>
-          <Col md="10">
-            <Field
-                name='mode'
-                as='select'
-                className='form-control'>
-                  <option>Select...</option>
-                  <option>remote</option>
-                  <option>onsite</option>
-                  <option>hybrid</option>
-            </Field>
-            <ErrorMessage name="mode">
-                {(msg) => <p className="text-danger">{msg}</p>}
-            </ErrorMessage>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label htmlFor="company" md="2">
-            Company
-          </Label>
-          <Col md="10">
-            <Field 
-                name='company'
-                placeholder='Company Name'
-                className="form-control" />
-            <ErrorMessage name="company">
-                {(msg) => <p className="text-danger">{msg}</p>}
-            </ErrorMessage>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label htmlFor="status" md="2">
-            Application Status
-          </Label>
-          <Col md="10">
-            <Field
-                name='status'
-                as='select'
-                className='form-control'>
-                  <option>Select...</option>
-                  <option>Resume submitted</option>
-                  <option>Online Accessment</option>
-                  <option>Interview 1</option>
-                  <option>Interview 2</option>
-            </Field>
-            <ErrorMessage name="status">
-                {(msg) => <p className="text-danger">{msg}</p>}
-            </ErrorMessage>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label htmlFor="type" md="2">
-            Job Type
-          </Label>
-          <Col md="10">
-            <Field
-                name='type'
-                as='select'
-                className='form-control'>
-                  <option>Select...</option>
-                  <option>Full time</option>
-                  <option>Part time</option>
-                  <option>Contract</option>
-            </Field>
-            <ErrorMessage name="type">
-                {(msg) => <p className="text-danger">{msg}</p>}
-            </ErrorMessage>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label htmlFor="skills" md="2">
-            Preferred skills
-          </Label>
-          <Col md="10">
-            <SkillsCheckboxGroup name="skills" />
-            <ErrorMessage name="skills">
-              {(msg) => <p className="text-danger">{msg}</p>}
-            </ErrorMessage>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label htmlFor="jobLink" md="2">
-            Job link
-          </Label>
-          <Col md="10">
-            <Field 
-                name='jobLink'
-                placeholder='Job Link'
-                className="form-control" />
-            <ErrorMessage name="jobLink">
-                {(msg) => <p className="text-danger">{msg}</p>}
-            </ErrorMessage>
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-            <Col md={{ size: 10, offset: 2 }}>
-                <Button type='submit' color='primary'>
-                    Add Job Application
-                </Button>
-            </Col>
-        </FormGroup>
-      </Form>
-    </Formik>
-  );
+    // when user closes the modal, navigate to home page
+    const toggleModal = () => {
+        setModal(!modal);
+        if (modal) {
+            navigate('/');
+        }
+    }
+
+    return (
+        <>
+            <Modal isOpen={modal} toggle={toggleModal}>
+                <ModalHeader toggle={toggleModal}>Success</ModalHeader>
+                <ModalBody>
+                    New job application entered successfully!
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggleModal}>OK, go to Home page</Button>
+                </ModalFooter>
+            </Modal>
+            <Formik
+                initialValues={{
+                    name: "",
+                    mode: "",
+                    company: "",
+                    status: "",
+                    type: "",
+                    skills: [],
+                    jobLink: ""
+                }}
+                onSubmit={handleSubmit}
+                validate={validateAddApplicationForm}
+            >
+                <Form>
+                    <FormGroup row>
+                        <Label htmlFor="name" md="2">
+                            Position Name
+                        </Label>
+                        <Col md="10">
+                            <Field 
+                                name='name'
+                                placeholder='Position Name'
+                                className="form-control" />
+                            <ErrorMessage name="name">
+                                {(msg) => <p className="text-danger">{msg}</p>}
+                            </ErrorMessage>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label htmlFor="mode" md="2">
+                            Mode
+                        </Label>
+                        <Col md="10">
+                            <Field
+                                name='mode'
+                                as='select'
+                                className='form-control'>
+                                <option>Select...</option>
+                                <option>remote</option>
+                                <option>onsite</option>
+                                <option>hybrid</option>
+                            </Field>
+                            <ErrorMessage name="mode">
+                                {(msg) => <p className="text-danger">{msg}</p>}
+                            </ErrorMessage>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label htmlFor="company" md="2">
+                            Company
+                        </Label>
+                        <Col md="10">
+                            <Field 
+                                name='company'
+                                placeholder='Company Name'
+                                className="form-control" />
+                            <ErrorMessage name="company">
+                                {(msg) => <p className="text-danger">{msg}</p>}
+                            </ErrorMessage>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label htmlFor="status" md="2">
+                            Application Status
+                        </Label>
+                        <Col md="10">
+                            <Field
+                                name='status'
+                                as='select'
+                                className='form-control'>
+                                <option>Select...</option>
+                                <option>Resume submitted</option>
+                                <option>Online Accessment</option>
+                                <option>Interview 1</option>
+                                <option>Interview 2</option>
+                            </Field>
+                            <ErrorMessage name="status">
+                                {(msg) => <p className="text-danger">{msg}</p>}
+                            </ErrorMessage>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label htmlFor="type" md="2">
+                            Job Type
+                        </Label>
+                        <Col md="10">
+                            <Field
+                                name='type'
+                                as='select'
+                                className='form-control'>
+                                <option>Select...</option>
+                                <option>Full time</option>
+                                <option>Part time</option>
+                                <option>Contract</option>
+                            </Field>
+                            <ErrorMessage name="type">
+                                {(msg) => <p className="text-danger">{msg}</p>}
+                            </ErrorMessage>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label htmlFor="skills" md="2">
+                            Preferred skills
+                        </Label>
+                        <Col md="10">
+                            <SkillsCheckboxGroup name="skills" />
+                            <ErrorMessage name="skills">
+                                {(msg) => <p className="text-danger">{msg}</p>}
+                            </ErrorMessage>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label htmlFor="jobLink" md="2">
+                            Job link
+                        </Label>
+                        <Col md="10">
+                            <Field 
+                                name='jobLink'
+                                placeholder='Job Link'
+                                className="form-control" />
+                            <ErrorMessage name="jobLink">
+                                {(msg) => <p className="text-danger">{msg}</p>}
+                            </ErrorMessage>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Col md={{ size: 10, offset: 2 }}>
+                            <Button type='submit' color='primary'>
+                                Add Job Application
+                            </Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+            </Formik>
+        </>
+    );
 }
 
 export default AddApplicationForm
